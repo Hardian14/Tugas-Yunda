c3fc3acfd3859e6575256fd27a4792183099d71c
wahyunda <95113685+wahyunda@users.noreply.github.com>
2022-01-28 21:02:02 +0700

    Folder latihan 28/01
    latihan 28/01

---
+++ latihan/lt1.php
@@ -0,0 +1,66 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    
+    <!-- ini adalah icon halaman -->
+    <link rel="shortcut icon" href="images/images.png">
+    <!-- link tambahan (font) -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Licorice&family=Neonderthaw&display=swap" rel="stylesheet"> 
+    <link href="https://fonts.googleapis.com/css2?family=Licorice&family=Moon+Dance&family=Neonderthaw&display=swap" rel="stylesheet"> 
+ 
+    <!-- link awasome -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.13.1/css/all.css" integrity="sha384-xxzQGERXS00kBmZW/6qxqJPyxW3UR0BPsL4c8ILaIWXva5kFi7TxkIIaMiKtqV1Q" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+
+    <style>
+      .paragraf1 {
+        font-size : 48px;
+        font-family: 'Licorice', cursive;
+        font-family: 'Neonderthaw', cursive;
+      }
+    
+      .paragraf2 {
+        font-size : 48px;
+        font-family: 'Licorice', cursive;
+        font-family: 'Moon Dance', cursive;
+      
+      }
+
+    </style>
+   
+  </head>
+  <body>
+    <div class="container">
+      <div class="paragraf1">
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dignissimos magnam iusto cum itaque quo ipsam, dolore delectus nesciunt molestias obcaecati voluptates excepturi illo odit voluptas, nihil, tempora quos possimus ratione!</p>
+      </div>
+      <div class="paragraf2">
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aut quas officia quam delectus pariatur iste necessitatibus aliquid unde sapiente nobis magnam laboriosam, aperiam sed quia, tenetur illo! Nobis, accusantium culpa.</p>
+      </div>
+      <!-- nbsp untuk memberikan 1 karakter spaci -->
+      <i class="fab fa-instagram" style = "color : #f296f2 "></i> &nbsp; &nbsp; Instagram <br> <br>
+      <i class="fab fa-facebook"></i> &nbsp; &nbsp; Facebook
+
+      <!-- ini untuk memberi warna dan halaman berbeda -->
+      <!-- <li><a href="https://www.detik.com" target = "_blank" ><i class="fa fa-envelope" style = "color : blue"></i></a></li> -->
+
+    </div>
+    
+  
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </body>
+</html>
