8adb0a9b81805d94d6eefa6efe8a00dc30124212
wahyunda <95113685+wahyunda@users.noreply.github.com>
2022-02-01 22:47:51 +0700

    tugas 1 02/01
    tugas 1 02/01

---
+++ tugas/yunda_tugas1.php
@@ -0,0 +1,180 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <!-- link font family -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Licorice&family=Neonderthaw&display=swap" rel="stylesheet"> 
+
+    <link href="https://fonts.googleapis.com/css2?family=Comforter&family=Dancing+Script:wght@700&family=Indie+Flower&family=Licorice&family=Moon+Dance&family=Neonderthaw&family=Permanent+Marker&family=Poppins:wght@100;300&family=Quintessential&family=Short+Stack&family=Supermercado+One&family=Syne+Tactile&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Comforter&family=Dancing+Script:wght@700&family=Indie+Flower&family=Licorice&family=Moon+Dance&family=Neonderthaw&family=Permanent+Marker&family=Poppins:wght@100;300&family=Quintessential&family=Shadows+Into+Light&family=Short+Stack&family=Supermercado+One&family=Syne+Tactile&display=swap" rel="stylesheet"> 
+
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.13.1/css/all.css" integrity="sha384-xxzQGERXS00kBmZW/6qxqJPyxW3UR0BPsL4c8ILaIWXva5kFi7TxkIIaMiKtqV1Q" crossorigin="anonymous">
+
+    <link rel="shortcut icon" href="images/20170803131.jpg">
+
+<!-- catatan pribadi font family -->
+<!-- font-family: 'Comforter', cursive;
+font-family: 'Dancing Script', cursive;
+font-family: 'Indie Flower', cursive;
+font-family: 'Licorice', cursive;
+font-family: 'Moon Dance', cursive;
+font-family: 'Neonderthaw', cursive;
+font-family: 'Permanent Marker', cursive;
+font-family: 'Poppins', sans-serif;
+font-family: 'Quintessential', cursive;
+font-family: 'Short Stack', cursive;
+font-family: 'Supermercado One', cursive;
+font-family: 'Syne Tactile', cursive; -->
+
+    <style>
+    @media only screen and (min-width : 0px) and (max-width : 400px) {
+      .paragraf1 {
+        font-family: 'Licorice', cursive;
+        font-family: 'Neonderthaw', cursive;
+        font-size : 20px;
+        color : #2f72de;
+      }
+    
+      .paragraf2 {
+        font-family: 'Shadows Into Light', cursive;
+        font-size : 20px;
+        color : #1ab1e8;
+      }
+
+      .paragraf3 {
+        font-family: 'Dancing Script', cursive; 
+        font-size : 20px;
+        color : #0bb8a6;
+      }
+    }
+
+    @media only screen and (min-width : 401px) and (max-width : 600px) {
+      .paragraf1 {
+        font-family: 'Licorice', cursive;
+        font-family: 'Neonderthaw', cursive;
+        font-size : 40px;
+        color : #1c22d9;
+      }
+    
+      .paragraf2 {
+        font-family: 'Shadows Into Light', cursive;
+        font-size : 40px;
+        color : #2d2f80 ;
+      }
+
+      .paragraf3 {
+        font-family: 'Dancing Script', cursive; 
+        font-size : 40px;
+        color : #3538f2;
+      }
+    }
+
+    @media only screen and (min-width : 601px) and (max-width : 800px) {
+      .paragraf1 {
+        font-family: 'Licorice', cursive;
+        font-family: 'Neonderthaw', cursive;
+        font-size : 60px;
+        color : #df61ed;
+      }
+    
+      .paragraf2 {
+        font-family: 'Shadows Into Light', cursive;
+        font-size : 60px;
+        color : #bd33cc ;
+      }
+
+      .paragraf3 {
+        font-family: 'Dancing Script', cursive; 
+        font-size : 60px;
+        color : #900e9e;
+      }
+    }
+
+    @media only screen and (min-width : 801px) and (max-width : 1000px) {
+      .paragraf1 {
+        font-family: 'Licorice', cursive;
+        font-family: 'Neonderthaw', cursive;
+        font-size : 80px;
+        color : #bee663;
+      }
+    
+      .paragraf2 {
+        font-family: 'Shadows Into Light', cursive;
+        font-size : 80px;
+        color : #82a82a ;
+      }
+
+      .paragraf3 {
+        font-family: 'Dancing Script', cursive; 
+        font-size : 80px;
+        color : #94d10a;
+      }
+    }
+
+    @media only screen and (min-width : 1001px) {
+      .paragraf1 {
+        font-family: 'Licorice', cursive;
+        font-family: 'Neonderthaw', cursive;
+        font-size : 80px;
+        color : #585c51;
+      }
+    
+      .paragraf2 {
+        font-family: 'Shadows Into Light', cursive;
+        font-size : 80px;
+        color : #82a82a ;
+      }
+
+      .paragraf3 {
+        font-family: 'Dancing Script', cursive; 
+        font-size : 80px;
+        color : #94d10a;
+      }
+    }
+
+    </style>
+
+
+    <title>Tugas Yunda</title>
+  </head>
+  <body>
+  <div class="container">  
+    <div class="paragraf1">
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsum, excepturi obcaecati? Voluptate obcaecati quos, cumque possimus corporis commodi? Nihil optio aliquam corrupti sint. Minus debitis non dolorem vitae provident praesentium.</p>
+    </div>
+
+    <div class="paragraf2">
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magnam natus ab dolorem nihil perferendis, esse aut voluptates deserunt reiciendis nulla ad saepe facere, omnis voluptatibus blanditiis facilis dicta officia sunt.</p>
+    </div>
+
+    <div class="paragraf3">
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia dignissimos expedita quam perspiciatis quo similique consequatur facilis distinctio magni exercitationem hic, minus quis laboriosam, quas, reiciendis sunt numquam! Quos, sit?</p>
+    </div>
+
+<!-- Unordered list dibuat dengan tag <ul> dan untuk item-nya dibuat juga dengan tag <li>. -->
+    <ul style = "font-size : 40px">
+
+      <li style= "list-style-type: none"> <i class="fab fa-youtube" style = "color : #b01328"></i> Youtube </li>
+      <li style= "list-style-type: none"> <i class="fab fa-telegram" style = "color : #1e7dbd"></i> Telegram </li> 
+      <li style= "list-style-type: none"> <i class="fab fa-whatsapp" style = "color : #11ed14"></i> Whatsapp </li>
+      <li style= "list-style-type: none"> <i class="fab fa-github" style = "color : #423d70"></i> Github </li>
+   
+    </ul>
+  </div>
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </body>
+</html>
